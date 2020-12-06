d7cb97845978b400c93f098e1b8af0289c96d0cf
Saad <saadmasood96@hotmail.com>
2020-12-06 18:52:51 -0500

    styling for Footer center controls

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,11 +1,28 @@
 import React from 'react';
+import PlayCircleOutlineIcon from '@material-ui/icons/PlayCircleOutline';
+import SkipPreviousIcon from '@material-ui/icons/SkipPrevious';
+import SkipNextIcon from '@material-ui/icons/SkipNext';
+import ShuffleIcon from '@material-ui/icons/Shuffle';
+import RepeatIcon from '@material-ui/icons/Repeat';
 
 import './Footer.css';
 
 const Footer = () => {
   return (
     <footer className="footer">
-      Footer
+      <div className="footer_left">
+        <p>Album and song details</p>
+      </div>
+      <div className="footer_center">
+        <ShuffleIcon className="footer_green" />
+        <SkipPreviousIcon className="footer_icon" />
+        <PlayCircleOutlineIcon className="footer_icon" fontSize="large" />
+        <SkipNextIcon className="footer_icon" />
+        <RepeatIcon className="footer_green" />
+      </div>
+      <div className="footer_right">
+        <p>Volume controls</p>
+      </div>
     </footer>
   );
 };
