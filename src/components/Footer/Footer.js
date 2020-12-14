1de24c5b5f75fcff660eeb99c419201b655a8193
Saad <saadmasood96@hotmail.com>
2020-12-14 14:29:06 -0500

    UI for left-side of footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -4,6 +4,9 @@ import SkipPreviousIcon from '@material-ui/icons/SkipPrevious';
 import SkipNextIcon from '@material-ui/icons/SkipNext';
 import ShuffleIcon from '@material-ui/icons/Shuffle';
 import RepeatIcon from '@material-ui/icons/Repeat';
+import PlaylistPlayIcon from '@material-ui/icons/PlaylistPlay';
+import VolumeDownIcon from '@material-ui/icons/VolumeDown';
+import { Grid, Slider } from '@material-ui/core';
 
 import './Footer.css';
 
@@ -14,14 +17,24 @@ const Footer = () => {
         <p>Album and song details</p>
       </div>
       <div className="footer_center">
-        <ShuffleIcon className="footer_green" />
+        <ShuffleIcon className="footer_icon" />
         <SkipPreviousIcon className="footer_icon" />
         <PlayCircleOutlineIcon className="footer_icon" fontSize="large" />
         <SkipNextIcon className="footer_icon" />
-        <RepeatIcon className="footer_green" />
+        <RepeatIcon className="footer_icon" />
       </div>
       <div className="footer_right">
-        <p>Volume controls</p>
+      <Grid container spacing={2}>
+          <Grid item>
+            <PlaylistPlayIcon  className="footer_icon" />
+          </Grid>
+          <Grid item>
+            <VolumeDownIcon className="footer_icon" />
+          </Grid>
+          <Grid item xs>
+            <Slider/>
+          </Grid>
+        </Grid>
       </div>
     </footer>
   );
