413cb5a19db5ec0306a54fe7d015c53690e6ab13
Saad <saadmasood96@hotmail.com>
2021-01-03 15:53:42 -0500

    styling for Body

--- src/components/Body/Body.js
+++ src/components/Body/Body.js
@@ -8,6 +8,17 @@ const Body = ({ spotifyApi }) => {
   return (
     <section className="body">
       <Header spotifyApi={spotifyApi} />
+      <div className="body_info">
+        <img
+          src="https://i.scdn.co/image/11258b6c69204820d79575e0587f415735db2350"
+          alt="Discover Weekly picture"
+        />
+        <div className="body_info_text">
+          <strong>PLAYLIST</strong>
+          <h2>Discover Weekly</h2>
+          <p>Description...</p>
+        </div>
+      </div>
     </section>
   );
 };
